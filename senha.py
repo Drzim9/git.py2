dbcdcc6044b405a2f87dfef032d754479b4d3723
Daniel Magalhães <164816634+Drzim9@users.noreply.github.com>
2024-12-21 20:35:02 -0300

    senha

---
+++ senha.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def gerar_senha(tamanho=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+# Exemplo de uso
+tamanho = int(input("Digite o tamanho da senha que deseja: "))
+senha_gerada = gerar_senha(tamanho)
+print(f"Sua senha gerada é: {senha_gerada}")
